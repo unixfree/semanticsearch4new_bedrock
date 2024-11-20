400e333789b951f75216265639edca7a615105ba
unixfree <kwangrag.son@gmail.com>
2024-11-20 11:36:41 +0900

    Update search_vector.py

--- search_vector.py
+++ search_vector.py
@@ -104,7 +104,7 @@ def hybrid_vector_search_with_sql(cluster, article_vector, title_vector, title_t
     try:
         # N1QL을 사용한 KNN 및 필터 검색
         query = f"""
-        SELECT title, date, author, url, like_count, SEARCH_SCORE() AS score
+        SELECT title, date, author, url, like_count, search_score() AS score
         FROM `travel-sample`.semantic.article AS t1
         WHERE author like "%기자"
         AND like_count >= 1
